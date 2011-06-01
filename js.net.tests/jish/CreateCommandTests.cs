b78bc326743ed9121de33b30a48436de8b97875a
Guido Tapia <guido@tapia.com.au>
2011-06-01 20:19:24 +1000

    Updated todo and adding breaking test that shows ILmerge stuff is not yet working

--- js.net.tests/jish/CreateCommandTests.cs
+++ js.net.tests/jish/CreateCommandTests.cs
@@ -3,9 +3,11 @@ using NUnit.Framework;
 
 namespace js.net.tests.jish
 {
-  [TestFixture] class CreateCommandTests : AbstractJishTest 
+  [TestFixture]
+  internal class CreateCommandTests : AbstractJishTest
   {
-    [Test] public void TestCreateFileInfoSuccess()
+    [Test]
+    public void TestCreateFileInfoSuccess()
     {
       const string file = @"..\\..\\..\\lib\\PicNet2.dll";
       jish.ExecuteCommand("var dllFile = jish.create('System.IO.FileInfo', '" + file + "');");
@@ -13,28 +15,44 @@ namespace js.net.tests.jish
       Assert.AreEqual(new FileInfo(file).FullName, console.GetLastMessage());
     }
 
-    [Test] public void TestNoArgsConstructor()
+    [Test]
+    public void TestCreateWithMultiplePossibleConstructors()
     {
-      jish.ExecuteCommand(@"jish.assembly('js.net.tests.dll')");      
-      jish.ExecuteCommand("var test = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests');");
-      jish.ExecuteCommand("console.log(test.ConstructorType);");
-      Assert.AreEqual("No Args", console.GetLastMessage());
+      jish.ExecuteCommand(@"jish.assembly('js.net.tests.dll')");
+      jish.ExecuteCommand("var test = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests', 'str1', 'str2');");
+      jish.ExecuteCommand("console.log(test.GetConstructorType());");
+      Assert.AreEqual("String[str1] and String[str2] Arg", console.GetLastMessage());
     }
 
-    [Test] public void TestCreateWithMultipleconstructorArgs()
+    [Test]
+    public void TestCreateWithMultipleconstructorArgs()
     {
-      jish.ExecuteCommand(@"jish.assembly('js.net.tests.dll')");      
+      jish.ExecuteCommand(@"jish.assembly('js.net.tests.dll')");
       jish.ExecuteCommand("var test = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests', 'str', '1');");
       jish.ExecuteCommand("console.log(test.ConstructorType);");
       Assert.AreEqual("String[str] and Int[1] Arg", console.GetLastMessage());
     }
 
-    [Test] public void TestCreateWithMultiplePossibleConstructors()
+    [Test]
+    public void TestMultipleInstances()
     {
-      jish.ExecuteCommand(@"jish.assembly('js.net.tests.dll')");      
-      jish.ExecuteCommand("var test = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests', 'str1', 'str2');");
-      jish.ExecuteCommand("console.log(test.GetConstructorType());");
-      Assert.AreEqual("String[str1] and String[str2] Arg", console.GetLastMessage());
+      jish.ExecuteCommand(
+        @"
+jish.assembly('js.net.tests.dll');
+var x1 = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests', '1', '1');
+var x2 = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests', '2', '2');
+console.log('x1 - ' + x1.GetConstructorType() + ' x2 - ' + x2.GetConstructorType());
+");
+      Assert.AreEqual("x1 - String[1] and Int[1] Arg x2 - String[2] and Int[2] Arg", console.GetLastMessage());
+    }
+
+    [Test]
+    public void TestNoArgsConstructor()
+    {
+      jish.ExecuteCommand(@"jish.assembly('js.net.tests.dll')");
+      jish.ExecuteCommand("var test = jish.create('js.net.tests.jish.TestCreateTarget, js.net.tests');");
+      jish.ExecuteCommand("console.log(test.ConstructorType);");
+      Assert.AreEqual("No Args", console.GetLastMessage());
     }
   }
 
@@ -54,10 +72,14 @@ namespace js.net.tests.jish
 
     public TestCreateTarget(string strArg1, string strArg2)
     {
-      constructorType = "String[" + strArg1 +"] and String[" + strArg2 +"] Arg";
+      constructorType = "String[" + strArg1 + "] and String[" + strArg2 + "] Arg";
     }
 
-    public string ConstructorType { get { return constructorType; }}
+    public string ConstructorType
+    {
+      get { return constructorType; }
+    }
+
     public string GetConstructorType()
     {
       return constructorType;
---
+++ js.net.tests/jish/JishNuGetDeploymentPackageTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace js.net.tests.jish
+{
+  [TestFixture] public class JishNuGetDeploymentPackageTest
+  {
+    private const string jishBuildDir = @"..\..\..\build\jish\tools\";
+    private const string testFile = "test.js";
+    private const string testContent = 
+@"
+var file = jish.create('System.IO.File');
+if (file.Exists('test.js')) {
+  console.log('success');
+} else {
+  console.log('fail');
+}
+";
+
+    [SetUp] public void SetUp()
+    {
+      if (File.Exists(testFile))  File.Delete(testFile);
+      File.WriteAllText(testFile, testContent);
+    }
+
+    [TearDown] public void TearDown()
+    {
+      if (File.Exists(testFile))  File.Delete(testFile);
+    }
+
+    [Test] public void TestRunSimpleScriptInOwnProcess()
+    {
+      AssertBuildDirectoryExistsAndAppearsCorrect();
+
+
+      const string command = jishBuildDir + "jish.exe";
+      const string arguments = testFile;
+      using (var process = new Process
+                             {
+                               StartInfo =
+                                 {
+                                   FileName = command,
+                                   Arguments = arguments,
+                                   UseShellExecute = false,
+                                   RedirectStandardOutput = true,
+                                   RedirectStandardError = true
+                                 }
+                             })
+      {
+        process.Start();
+        string err = process.StandardError.ReadToEnd();
+        string output = process.StandardOutput.ReadToEnd();
+        Assert.IsEmpty(err);
+        Assert.AreEqual("success", output);
+        process.WaitForExit();
+        Assert.AreEqual(0, process.ExitCode);
+      }
+    }
+
+    private void AssertBuildDirectoryExistsAndAppearsCorrect()
+    {
+      Assert.IsTrue(Directory.Exists(jishBuildDir));
+      string[] files = Directory.GetFiles(jishBuildDir, "*.exe");
+      Assert.IsTrue(files.Single(f => f.EndsWith("jish.exe")) != null);
+      files = Directory.GetFiles(jishBuildDir, "*.dll");
+      Assert.IsTrue(files.Single(f => f.EndsWith("Noesis.Javascript.dll")) != null);
+    }
+  }
+}
