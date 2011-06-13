e3806e3faaaa0ea7d62f1aafdead7c684d2a32f6
Guido Tapia <guido@tapia.com.au>
2011-06-13 14:07:09 +1000

    WIP: Delegate support

---
+++ js.net.tests/jish/TypeILWrapper/AbstractTypeILWrapperTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using js.net.jish.IL;
+using js.net.Util;
+
+namespace js.net.tests.jish.TypeILWrapper
+{
+  public abstract class AbstractTypeILWrapperTest {
+    
+    static AbstractTypeILWrapperTest()
+    {
+      Trace.Listeners.Clear();
+      Trace.Listeners.Add(new ExceptionTraceListener());
+    }
+
+    private readonly net.jish.IL.TypeILWrapper wrapper = new net.jish.IL.TypeILWrapper();
+    private readonly Type typeToWrap;
+
+    protected AbstractTypeILWrapperTest(Type typeToWrap) { this.typeToWrap = typeToWrap; }
+
+    protected IEnumerable<MethodInfo> GetExpandedMethods(string realMethodName, string wrappedMethodName = null, object instace = null)
+    {
+      object wrapped = wrapper.CreateWrapper(typeToWrap, 
+        new [] {new MethodToProxify(typeToWrap.GetMethod(realMethodName), instace)});
+      return wrapped.GetType().GetMethods().Where(mi => mi.Name.Equals(wrappedMethodName ?? realMethodName)).OrderBy(mi => mi.GetParameters().Length);
+    }
+  }
+}
